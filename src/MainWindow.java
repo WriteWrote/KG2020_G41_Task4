3499591410712c83c6e2bc88896f48d0d491b127
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2020-12-18 18:24:55 +0300

    Initial commit

---
+++ src/MainWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MainWindow extends JDialog {
+    private JPanel contentPane;
+
+    public MainWindow() {
+        setContentPane(contentPane);
+        setModal(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow dialog = new MainWindow();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
